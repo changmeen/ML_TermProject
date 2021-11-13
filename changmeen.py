95447b8d33f3bd931af03732453a5f4dcaddd3f4
ShinMinOh <luciano97@naver.com>
2021-11-13 17:41:47 +0900

    1. selectKbest 코드 changmeen.py에 추가 2.시각화 코드는 모두 temp.py로 뺌 3.시각화 일부 수정

--- changmeen.py
+++ changmeen.py
@@ -51,117 +51,33 @@ def outlier(df, feature):
 
 df = pd.read_csv('Banking_churn_prediction.csv')
 
+##Feature Selection(Using Select-KBest)-----------------
+df = df.fillna(method='ffill')
+df = df.drop(['last_transaction'], axis=1)
 
-# def numerical_vis_func(x):
-#     plt.hist(x,bins=10)
-#     plt.show()
+feature_names = list(df.select_dtypes(object))
 
-# ##6가지 column들 시각화 
-# column_name_numerical=['vintage','age','dependents','city','customer_nw_category','branch_code'] 
+encoder = LabelEncoder()
+for i in feature_names:
+    encoded = pd.DataFrame(encoder.fit_transform(df[i]), columns=[i])
+    #print(encoded)
+    df.drop(columns=i, inplace=True)
+    df = pd.concat((df, encoded), axis=1)
+    
+X = df.drop(['churn'], axis=1)
+y = df['churn']
 
-# for i in range(len(column_name_numerical)):
-#     plt.title(column_name_numerical[i])
-#     plt.xlabel(column_name_numerical[i])    
-#     plt.ylabel('Count')
-#     if(i==2):
-#         list=[0,1,2,3,4,5]
-#         plt.hist(df['dependents'],list)
-#         plt.show()
-#         continue;
-#     if(i==4):
-#         list=[0,1,2,3,4,5]
-#         plt.hist(df['customer_nw_category'],list)
-#         plt.show()
-#         continue;
-#     numerical_vis_func(df[[column_name_numerical[i]]])
+selector = SelectKBest(score_func=f_classif, k=19)
+fit = selector.fit(X, y)
 
+dfcolumns = pd.DataFrame(X.columns)
+dfscores = pd.DataFrame(fit.scores_)
 
-# ##3가지 categorical data 시각화 
-# column_name_category=['gender','occupation','churn']
-# for i in range(len(column_name_category)):
-#     plt.title(column_name_category[i])
-#     plt.xlabel(column_name_category[i])    
-#     plt.ylabel('Count')
-#     sns.countplot(df[column_name_category[i]])
-#     plt.show() 
+featureScores = pd.concat([dfcolumns, dfscores], axis=1)
+featureScores.columns = ['Spec', 'Score']
 
-
-# #Current value cloumns들 시각화 
-# #Current value visualization-------------------------------
-# column_name_current=['current_balance','current_month_credit','current_month_debit','current_month_balance']    
-# var_color_dict = {'current_balance': 'blue', 
-#                   'current_month_credit': 'red', 
-#                   'current_month_debit': 'yellow', 
-#                   'current_month_balance': 'green'}
-
-# i = [0, 0, 1, 1]
-# j = [0, 1, 0, 1]
-
-# f, axes = plt.subplots(2, 2, figsize=(8, 6),sharex=True)
-# for var, i, j in zip(var_color_dict, i, j):
-#     plt.xlabel(column_name_current[i])    
-#     sns.distplot(df[column_name_current[i]],color = var_color_dict[var],ax = axes[i, j])
-# plt.show()
-# #--------------------------------------------
-
-
-# ##average value cloumns들 시각화 
-# #average mothly balance value visualization-------------------------------
-# column_name_average=['average_monthly_balance_prevQ','average_monthly_balance_prevQ2']    
-# figure = plt.figure(figsize=(18,6))
-
-# ax1 = plt.subplot(1,2,1)
-# sns.distplot(df['average_monthly_balance_prevQ'], ax=ax1)
-# ax2 = plt.subplot(1,2,2)
-# sns.distplot(df['average_monthly_balance_prevQ2'], ax=ax2)    # 기본값은 kde(선) True, hist(막대) True
-# plt.show()
-# #--------------------------------------------
-
-# ##previous value cloumns들 시각화 
-# #previous value-------------------------------
-# column_name_previous=['previous_month_end_balance','previous_month_credit','previous_month_debit','previous_month_balance']    
-# var_color_dict2 = {'previous_month_end_balance': 'blue', 
-#                   'previous_month_credit': 'red', 
-#                   'previous_month_debit': 'yellow', 
-#                   'previous_month_balance': 'green'}
-
-# a = [0, 0, 1, 1]
-# b = [0, 1, 0, 1]
-
-# f, axes = plt.subplots(2, 2, figsize=(10, 8), sharex=True)
-# for var2, a, b in zip(var_color_dict2, a, b):
-#     sns.distplot(df[column_name_previous[a]],color = var_color_dict2[var2],ax = axes[a, b])
-
-# plt.show()
-# # ----------------------------------------
-
-
-# ##월단위로 추출한 Last transaction 시각화 
-# #----------Last transaction barplot with only month-----------------
-# # Creating an instance(data) of Datetimeindex class using last_transaction
-# temp_df=df.copy()
-# temp_df['last_transaction'] = pd.DatetimeIndex(temp_df['last_transaction'])
-
-# # There are only 3 values that last_transaction in 2018
-# # All other records are in 2019 so drop 2018 values
-# date = pd.DatetimeIndex(temp_df['last_transaction'])
-# indexNames = temp_df[date.year == 2018].index
-# temp_df.drop(indexNames, inplace=True)
-# temp_df = temp_df.reset_index(drop=True)
-
-# # Dealing with nan values in last_transaction
-# date = pd.DatetimeIndex(temp_df['last_transaction'])
-# temp_df['last_transaction'] = date.month
-# temp_df.dropna(axis=0,inplace=True)
-# print("last_transaction_month\n",temp_df['last_transaction'])
-
-# plt.title("Last transation_month")
-# plt.xlabel('Last transation_month')    
-# plt.ylabel('Count')
-# sns.countplot(temp_df['last_transaction'])
-# plt.show() 
-# #------------------------------------------------------
-# #------------------------------------------------------
+print(featureScores.nlargest(19, 'Score'))
+#---------------------------------------------------------
 
 
 # Convert churn, branch_code, customer_nw_category to category type
--- temp.py
+++ temp.py
@@ -10,41 +10,41 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 df = pd.read_csv("C:/Users/MOS/Desktop/Banking_churn_prediction.csv")
 
-# def numerical_vis_func(x):
-#     plt.hist(x,bins=10)
-#     plt.show()
+def numerical_vis_func(x):
+    plt.hist(x,bins=10)
+    plt.show()
 
-#6가지 column들 시각화 
-# column_name_numerical=['vintage','age','dependents','city','customer_nw_category','branch_code'] 
+###6가지 column들 시각화 
+column_name_numerical=['vintage','age','dependents','city','customer_nw_category','branch_code'] 
 
-# for i in range(len(column_name_numerical)):
-#     plt.title(column_name_numerical[i])
-#     plt.xlabel(column_name_numerical[i])    
-#     plt.ylabel('Count')
-#     if(i==2):
-#         list=[0,1,2,3,4,5]
-#         plt.hist(df['dependents'],list)
-#         plt.show()
-#         continue;
-#     if(i==4):
-#         list=[0,1,2,3,4,5]
-#         plt.hist(df['customer_nw_category'],list)
-#         plt.show()
-#         continue;
-#     numerical_vis_func(df[[column_name_numerical[i]]])
+for i in range(len(column_name_numerical)):
+    plt.title(column_name_numerical[i])
+    plt.xlabel(column_name_numerical[i])    
+    plt.ylabel('Count')
+    if(i==2):
+        list=[0,1,2,3,4,5]
+        plt.hist(df['dependents'],list)
+        plt.show()
+        continue;
+    if(i==4):
+        list=[0,1,2,3,4,5]
+        plt.hist(df['customer_nw_category'],list)
+        plt.show()
+        continue;
+    numerical_vis_func(df[[column_name_numerical[i]]])
 
 
-#3가지 categorical data 시각화 
-# column_name_category=['gender','occupation','churn']
-# for i in range(len(column_name_category)):
-#     plt.title(column_name_category[i])
-#     plt.xlabel(column_name_category[i])    
-#     plt.ylabel('Count')
-#     sns.countplot(df[column_name_category[i]])
-#     plt.show() 
+####3가지 categorical data 시각화 
+column_name_category=['gender','occupation','churn']
+for i in range(len(column_name_category)):
+    plt.title(column_name_category[i])
+    plt.xlabel(column_name_category[i])    
+    plt.ylabel('Count')
+    sns.countplot(df[column_name_category[i]])
+    plt.show() 
 
-#Current value cloumns들 시각화 
-#Current value visualization-------------------------------
+####Current value cloumns들 시각화 
+####Current value visualization-------------------------------
 column_name_current=['current_balance','current_month_credit','current_month_debit','current_month_balance']    
 var_color_dict = {'current_balance': 'blue', 
                   'current_month_credit': 'red', 
@@ -59,62 +59,77 @@ for var, i, j in zip(var_color_dict, i, j):
     plt.xlabel(column_name_current[i])    
     sns.distplot(df[column_name_current[i]],color = var_color_dict[var],ax = axes[i, j])
 plt.show()
-#--------------------------------------------
-#average value cloumns들 시각화 
-# #average mothly balance value visualization-------------------------------
-# column_name_average=['average_monthly_balance_prevQ','average_monthly_balance_prevQ2']    
-# figure = plt.figure(figsize=(18,6))
+####--------------------------------------------
 
-# ax1 = plt.subplot(1,2,1)
-# sns.distplot(df['average_monthly_balance_prevQ'], ax=ax1)
-# ax2 = plt.subplot(1,2,2)
-# sns.distplot(df['average_monthly_balance_prevQ2'], ax=ax2)    # 기본값은 kde(선) True, hist(막대) True
-# plt.show()
-# #--------------------------------------------
+####average value cloumns들 시각화 
+####average mothly balance value visualization-------------------------------
+column_name_average=['average_monthly_balance_prevQ','average_monthly_balance_prevQ2']    
+figure = plt.figure(figsize=(18,6))
 
-#previous value cloumns들 시각화 
-# #previous value-------------------------------
-# column_name_previous=['previous_month_end_balance','previous_month_credit','previous_month_debit','previous_month_balance']    
-# var_color_dict2 = {'previous_month_end_balance': 'blue', 
-#                   'previous_month_credit': 'red', 
-#                   'previous_month_debit': 'yellow', 
-#                   'previous_month_balance': 'green'}
+ax1 = plt.subplot(1,2,1)
+sns.distplot(df['average_monthly_balance_prevQ'], ax=ax1)
+ax2 = plt.subplot(1,2,2)
+sns.distplot(df['average_monthly_balance_prevQ2'], ax=ax2)    # 기본값은 kde(선) True, hist(막대) True
+plt.show()
+#####--------------------------------------------
 
-# a = [0, 0, 1, 1]
-# b = [0, 1, 0, 1]
+####previous value cloumns들 시각화 
+####previous value-------------------------------
+column_name_previous=['previous_month_end_balance','previous_month_credit','previous_month_debit','previous_month_balance']    
+var_color_dict2 = {'previous_month_end_balance': 'blue', 
+                  'previous_month_credit': 'red', 
+                  'previous_month_debit': 'yellow', 
+                  'previous_month_balance': 'green'}
 
-# f, axes = plt.subplots(2, 2, figsize=(10, 8), sharex=True)
-# for var2, a, b in zip(var_color_dict2, a, b):
-#     sns.distplot(df[column_name_previous[a]],color = var_color_dict2[var2],ax = axes[a, b])
+a = [0, 0, 1, 1]
+b = [0, 1, 0, 1]
 
-# plt.show()
-# # ----------------------------------------
+f, axes = plt.subplots(2, 2, figsize=(10, 8), sharex=True)
+for var2, a, b in zip(var_color_dict2, a, b):
+    sns.distplot(df[column_name_previous[a]],color = var_color_dict2[var2],ax = axes[a, b])
+
+plt.show()
+###----------------------------------------
 
 
-#월단위로 추출한 Last transaction 시각화 
-# #----------Last transaction barplot with only month-----------------
-# # Creating an instance(data) of Datetimeindex class using last_transaction
-# temp_df=df.copy()
-# temp_df['last_transaction'] = pd.DatetimeIndex(temp_df['last_transaction'])
+# ####월단위로 추출한 Last transaction 시각화 
+# ##----------Last transaction barplot with only month-----------------
+# ## Creating an instance(data) of Datetimeindex class using last_transaction
+temp_df=df.copy()
+temp_df['last_transaction'] = pd.DatetimeIndex(temp_df['last_transaction'])
 
 # # There are only 3 values that last_transaction in 2018
 # # All other records are in 2019 so drop 2018 values
-# date = pd.DatetimeIndex(temp_df['last_transaction'])
-# indexNames = temp_df[date.year == 2018].index
-# temp_df.drop(indexNames, inplace=True)
-# temp_df = temp_df.reset_index(drop=True)
+date = pd.DatetimeIndex(temp_df['last_transaction'])
+indexNames = temp_df[date.year == 2018].index
+temp_df.drop(indexNames, inplace=True)
+temp_df = temp_df.reset_index(drop=True)
 
-# # Dealing with nan values in last_transaction
-# date = pd.DatetimeIndex(temp_df['last_transaction'])
-# temp_df['last_transaction'] = date.month
-# temp_df.dropna(axis=0,inplace=True)
-# print("last_transaction_month\n",temp_df['last_transaction'])
+# Dealing with nan values in last_transaction
+date = pd.DatetimeIndex(temp_df['last_transaction'])
+temp_df['last_transaction'] = date.month
+temp_df.dropna(axis=0,inplace=True)
+print("last_transaction_month\n",temp_df['last_transaction'])
 
-# plt.title("Last transation_month")
-# plt.xlabel('Last transation_month')    
-# plt.ylabel('Count')
-# sns.countplot(temp_df['last_transaction'])
-# plt.show() 
-# #------------------------------------------------------
+plt.title("Last transation_month")
+plt.xlabel('Last transation_month')    
+plt.ylabel('Count')
+sns.countplot(temp_df['last_transaction'])
+plt.show() 
 
 
+# #Relation about (previous & current balance/credit/debit/mothly balance) & (Churn)  
+fig = plt.figure(figsize=(7,7))
+graph = sns.countplot(data=df, x='previous_month_end_balance', y='current_balance', hue='churn')
+plt.show()
+graph = sns.scatterplot(data=df, x='previous_month_credit', y='current_month_credit', hue='churn')
+plt.show()
+graph = sns.scatterplot(data=df, x='previous_month_debit', y='current_month_debit', hue='churn')
+plt.show()
+graph = sns.scatterplot(data=df, x='previous_month_balance', y='current_month_balance', hue='churn')
+plt.show()
+
+#relation with previous_month_debit & churn
+g=sns.FacetGrid(df,col='churn')
+g.map(plt.hist,'previous_month_debit',bins=20)
+plt.show()
